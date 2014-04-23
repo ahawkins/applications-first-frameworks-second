37802495232e8069d7ad62ebad937cc6e46d6498
ahawkins <adam@hawkins.io>
2014-04-23 14:57:04 -0500

    Make things green

--- test/publish_post_form_test.rb
+++ test/publish_post_form_test.rb
@@ -2,22 +2,22 @@ require_relative 'test_helper'
 
 class PublishPostFormTest < MiniTest::Unit::TestCase
   def test_requires_a_title
-    form = PublishPostForm.new title: nil
+    form = PublishPostForm.new title: nil, text: 'Foo'
 
     error = assert_raises ValidationError do
       form.validate
     end
 
-    assert_equal :title_blank, error.msg
+    assert_equal 'title_blank', error.message
   end
 
   def test_requires_text
-    form = PublishPostForm.new text: nil
+    form = PublishPostForm.new text: nil, title: 'Foo'
 
     error = assert_raises ValidationError do
       form.validate
     end
 
-    assert_equal :text_blank, error.msg
+    assert_equal 'text_blank', error.message
   end
 end
