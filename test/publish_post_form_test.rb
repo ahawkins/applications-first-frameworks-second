b6734a13f1e1aa7dd4da062103f78e94a61395c1
ahawkins <adam@hawkins.io>
2014-04-23 14:52:18 -0500

    Writing failing form tests

--- test/publish_post_form_test.rb
+++ test/publish_post_form_test.rb
@@ -1,7 +1,23 @@
 require_relative 'test_helper'
 
 class PublishPostFormTest < MiniTest::Unit::TestCase
-  def test_is_defined
-    assert PublishPostForm
+  def test_requires_a_title
+    form = PublishPostForm.new title: nil
+
+    error = assert_raises ValidationError do
+      form.validate
+    end
+
+    assert_equal :title_blank, error.msg
+  end
+
+  def test_requires_text
+    form = PublishPostForm.new text: nil
+
+    error = assert_raises ValidationError do
+      form.validate
+    end
+
+    assert_equal :text_blank, error.msg
   end
 end
