4ca7013a85fcc847917301d4466c8601f58f1fd4
ahawkins <adam@hawkins.io>
2014-04-23 15:49:30 -0500

    Get enough functionality in one step

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,16 @@ class PostsController < ApplicationController
   def new
     @form = PublishPostForm.new
   end
+
+  def create
+    form = PublishPostForm.new params[:post]
+    use_case = PublishPost.new form
+    post = use_case.execute
+
+    redirect_to post_path(id: post.id)
+  end
+
+  def show
+
+  end
 end
